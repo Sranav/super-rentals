7f63cbce8ed2d74d6ec3a5e327c2b171df5092ed
pranavliag <pranavmechary@gmail.com>
2020-08-13 20:38:06 +0530

    feat(component): add button events

--- app/components/share-button.js
+++ app/components/share-button.js
@@ -1,13 +1,15 @@
 import Component from '@glimmer/component';
 
-const TWEET_INTENT = 'https;//twitter.com/intent/tweet';
+const TWEET_INTENT = 'https://twitter.com/intent/tweet';
 
 export default class ShareButtonComponent extends Component {
   get currentURL() {
     return window.location.href;
   }
   get shareURL() {
+
     let url = new URL( TWEET_INTENT );
+
     url.searchParams.set( 'url', this.currentURL );
 
     if ( this.args.text ) {
