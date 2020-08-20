375475662420cc8067c51159effad66eaf72252a
pranavliag <pranavmechary@gmail.com>
2020-08-20 20:12:35 +0530

    feat(component): add query function

---
+++ app/components/rentals.js
@@ -0,0 +1,9 @@
+import Component from '@glimmer/component';
+import {
+  tracked
+} from '@glimmer/tracking';
+
+
+export default class RentalsComponent extends Component {
+  @tracked query = '';
+}
